f691074f5ef29708fd27e576b53dbd99cfac0b8e
Kiranpreet <kiranpreetsethi65@gmail.com>
2023-03-20 15:27:29 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import "./components/style.css"
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="flex justify-center align-middle">
+      Hello world!
+    </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/stateistics/statistics.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function statistics() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
